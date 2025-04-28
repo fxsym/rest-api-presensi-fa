eb11f5e6f6ffbb4cd2b60096ea3ce8b11367a4e6
fxsym <fsyamsudin24@gmail.com>
2025-04-29 01:11:24 +0700

    Menginstall artisan api, dan menambahkan route serta fungsi untuk login dan logout pada AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'user_input' => 'required',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        if (Str::contains($request->user_input, '@')) {
+            $user = User::where('email', $request->user_input)->first();
+        } else {
+            $user = User::where('username', $request->user_input)->first();
+        }
+
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'username' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken($request->device_name)->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login Succesfully',
+            'token' => $token
+        ], 200);
+    }
+
+    public function logout(Request $request) {
+        $request->user()->tokens()->delete();  //Revoke all tokens...
+        return response()->json([
+            'message' => 'Logout Succes'
+        ]);
+    }
+}
